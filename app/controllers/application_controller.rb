854bd4f5ab379b4d05b87badfab52ef71286e5e8
jambion <ambion_j@yahoo.com>
2017-06-23 02:15:58 +0000

    Added static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  def hello
+    render html: "Hello World! Welcome to the flex log!"
+  end
 end
+
